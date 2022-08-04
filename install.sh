687009a1d04564b04efcdffefee4a60bdfda368c
seaky shen <seaky.cn@gmail.com>
2022-08-04 10:24:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 mkdir -p ~/git && cd ~/git && git clone https://github.com/ntop/n2n.git -b 2.8-stable && cd n2n && ./autogen.sh && ./configure && make && sudo make install
 
 
-CONFIGFILE="/etc/n2n/edge.conf"
-[ ! -e $CONFIGFILE ] && sudo mkdir -p /etc/n2n && sudo bash -c "install -m 666 /dev/null $CONFIGFILE" && echo "- Make $CONFIGFILE" &&cat > $CONFIGFILE <<-EOF
--d=n2n0
--c=
--k=
+CONFIGFILE="/etc/n2/edge.conf"
+[ ! -e $CONFIGFILE ] && sudo mkdir -p /etc/n2 && sudo bash -c "install -m 666 /dev/null $CONFIGFILE" && echo "- Make $CONFIGFILE" &&cat > $CONFIGFILE <<-EOF
+-d=edge0
+-c=<community>
+-k=<password>
 -a=<ip>
 -l=<server>:<port>
 EOF
@@ -18,13 +18,13 @@ EOF
 SERVICEFILE="/etc/systemd/system/edge.service"
 [ ! -e $SERVICEFILE ] && sudo mkdir -p /etc/systemd/system && sudo bash -c "install -m 666 /dev/null $SERVICEFILE" && echo "- Make $SERVICEFILE" && cat > $SERVICEFILE <<-EOF
 [Unit]
-Description=n2n
+Description=n2
 After=network.target
 
 [Service]
 Type=simple
 User=root
-ExecStart=/usr/sbin/edge /etc/n2n/edge.conf -f
+ExecStart=/usr/sbin/edge /etc/n2/edge.conf -f
 
 [Install]
 WantedBy=multi-user.target
